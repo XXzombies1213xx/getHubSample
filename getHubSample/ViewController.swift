d7f33ccf7e8ce2299680b1cee848852387e77ccf
JIA POTEMPA <rl107520@rlas-macair-26.rlas-116.org>
2018-09-24 10:45:34 -0500

    Initial Commit

---
+++ getHubSample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  getHubSample
+//
+//  Created by JIA POTEMPA on 9/24/18.
+//  Copyright © 2018 JIA POTEMPA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
